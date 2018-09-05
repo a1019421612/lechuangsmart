c45924a5a39eda9bac82ace4666c78597b5a80aa
a1019421612 <1019421612@qq.com>
2018-09-05 19:28:59 +0800

    乐创UI更新

--- app/src/main/java/com/hbdiye/lechuangsmart/google/zxing/activity/CaptureActivity.java
+++ app/src/main/java/com/hbdiye/lechuangsmart/google/zxing/activity/CaptureActivity.java
@@ -38,11 +38,17 @@ import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 import com.hbdiye.lechuangsmart.R;
 import com.hbdiye.lechuangsmart.activity.AddCameraActivity;
+import com.hbdiye.lechuangsmart.activity.SeriesNumSearchActivity;
 import com.hbdiye.lechuangsmart.google.zxing.camera.CameraManager;
 import com.hbdiye.lechuangsmart.google.zxing.decoding.CaptureActivityHandler;
 import com.hbdiye.lechuangsmart.google.zxing.decoding.InactivityTimer;
 import com.hbdiye.lechuangsmart.google.zxing.decoding.RGBLuminanceSource;
 import com.hbdiye.lechuangsmart.google.zxing.view.ViewfinderView;
+import com.videogo.exception.BaseException;
+import com.videogo.exception.ExtraException;
+import com.videogo.util.ConnectionDetector;
+import com.videogo.util.LocalValidate;
+import com.videogo.util.LogUtil;
 
 import java.io.IOException;
 import java.util.Hashtable;
@@ -77,7 +83,10 @@ public class CaptureActivity extends AppCompatActivity implements Callback {
     public static final String INTENT_EXTRA_KEY_QR_SCAN = "qr_scan_result";
 
     private boolean isCamera;
-
+    private String mSerialNoStr = "";
+    private String mSerialVeryCodeStr = "";
+    private String deviceType = "";
+    private String TAG="CaptureActivity";
     /**
      * Called when the activity is first created.
      */
@@ -96,7 +105,11 @@ public class CaptureActivity extends AppCompatActivity implements Callback {
             iv_base_right.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    startActivity(new Intent(CaptureActivity.this, AddCameraActivity.class));
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("type", 0);
+                    Intent intent = new Intent(CaptureActivity.this, SeriesNumSearchActivity.class);
+                    intent.putExtras(bundle);
+                    CaptureActivity.this.startActivity(intent);
                 }
             });
         }
@@ -296,8 +309,9 @@ public class CaptureActivity extends AppCompatActivity implements Callback {
         } else {
             System.out.println("CaputreActivity= " + resultString);
             if (isCamera){
-                startActivity(new Intent(CaptureActivity.this,AddCameraActivity.class)
-                .putExtra("deviceInfo",resultString));
+
+                handleString(resultString);
+
             }else {
                 Intent resultIntent = new Intent();
                 Bundle bundle = new Bundle();
@@ -320,7 +334,130 @@ public class CaptureActivity extends AppCompatActivity implements Callback {
         }
 
     }
+    private void handleString(String resultString) {
+        // 初始化数据
+        mSerialNoStr = "";
+        mSerialVeryCodeStr = "";
+        deviceType = "";
+        LogUtil.errorLog(TAG, resultString);
+        String[] newlineCharacterSet = {
+                "\n\r", "\r\n", "\r", "\n"};
+        String stringOrigin = resultString;
+        // 寻找第一次出现的位置
+        int a = -1;
+        int firstLength = 1;
+        for (String string : newlineCharacterSet) {
+            if (a == -1) {
+                a = resultString.indexOf(string);
+                if (a > stringOrigin.length() - 3) {
+                    a = -1;
+                }
+                if (a != -1) {
+                    firstLength = string.length();
+                }
+            }
+        }
 
+        // 扣去第一次出现回车的字符串后，剩余的是第二行以及以后的
+        if (a != -1) {
+            resultString = resultString.substring(a + firstLength);
+        }
+        // 寻找最后一次出现的位置
+        int b = -1;
+        for (String string : newlineCharacterSet) {
+            if (b == -1) {
+                b = resultString.indexOf(string);
+                if (b != -1) {
+                    mSerialNoStr = resultString.substring(0, b);
+                    firstLength = string.length();
+                }
+            }
+        }
+
+        // 寻找遗失的验证码阶段
+        if (mSerialNoStr != null && b != -1 && (b + firstLength) <= resultString.length()) {
+            resultString = resultString.substring(b + firstLength);
+        }
+
+        // 再次寻找回车键最后一次出现的位置
+        int c = -1;
+        for (String string : newlineCharacterSet) {
+            if (c == -1) {
+                c = resultString.indexOf(string);
+                if (c != -1) {
+                    mSerialVeryCodeStr = resultString.substring(0, c);
+                }
+            }
+        }
+
+        // 寻找CS-C2-21WPFR 判断是否支持wifi
+        if (mSerialNoStr != null && c != -1 && (c + firstLength) <= resultString.length()) {
+            resultString = resultString.substring(c + firstLength);
+        }
+        if (resultString != null && resultString.length() > 0) {
+            deviceType = resultString;
+        }
+
+        if (b == -1) {
+            mSerialNoStr = resultString;
+        }
+
+        if (mSerialNoStr == null) {
+            mSerialNoStr = stringOrigin;
+        }
+        LogUtil.debugLog(TAG, "mSerialNoStr = " + mSerialNoStr + ",mSerialVeryCodeStr = " + mSerialVeryCodeStr
+                + ",deviceType = " + deviceType);
+        // 判断是不是9位
+        isValidate();
+    }
+
+    /**
+     * 判断是不是合法
+     *
+     * @throws
+     */
+    /** 本地数据合法性检测变量 */
+    private LocalValidate mLocalValidate = null;
+    private void isValidate() {
+        mLocalValidate = new LocalValidate();
+        try {
+            mLocalValidate.localValidatSerialNo(mSerialNoStr);
+            LogUtil.infoLog(TAG, mSerialNoStr);
+        } catch (BaseException e) {
+            handleLocalValidateSerialNoFail(e.getErrorCode());
+            LogUtil.errorLog(TAG, "searchCameraBySN-> local validate serial no fail, errCode:" + e.getErrorCode());
+            return;
+        }
+
+        if (!ConnectionDetector.isNetworkAvailable(this)) {
+            SmartToast.show(getResources().getString(R.string.query_camera_fail_network_exception));
+            return;
+        }
+        Bundle bundle = new Bundle();
+        bundle.putInt("type", 1);
+        bundle.putString("SerialNo", mSerialNoStr);
+        bundle.putString("very_code", mSerialVeryCodeStr);
+        bundle.putString("device_type", deviceType);
+        LogUtil.debugLog(TAG, "very_code:" + mSerialVeryCodeStr);
+        Intent intent = new Intent(CaptureActivity.this, SeriesNumSearchActivity.class);
+        intent.putExtras(bundle);
+        CaptureActivity.this.startActivity(intent);
+    }
+    private void handleLocalValidateSerialNoFail(int errCode) {
+        switch (errCode) {
+            case ExtraException.SERIALNO_IS_NULL:
+                SmartToast.show(getResources().getString(R.string.serial_number_is_null));
+                break;
+            case ExtraException.SERIALNO_IS_ILLEGAL:
+                // showToast(R.string.serial_number_is_illegal);
+                SmartToast.show("设备编号不合法");
+                break;
+            default:
+                SmartToast.show(getResources().getString(R.string.serial_number_error));
+                LogUtil.errorLog(TAG, "handleLocalValidateSerialNoFail-> unkown error, errCode:" + errCode);
+                break;
+        }
+    }
     private void initCamera(SurfaceHolder surfaceHolder) {
         try {
             CameraManager.get().openDriver(surfaceHolder);
